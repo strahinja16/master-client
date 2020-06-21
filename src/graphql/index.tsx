a54b9d406d298b426f3c3ef9640c6496065d73ed
Strahinja Laktovic <strahinja.laktovic@gmail.com>
2020-06-21 11:34:28 +0200

    change domain

--- src/graphql/index.tsx
+++ src/graphql/index.tsx
@@ -2,7 +2,7 @@ import ApolloClient, { InMemoryCache } from 'apollo-boost';
 
 export const client = new ApolloClient({
     cache: new InMemoryCache(),
-    uri: 'http://mini-mes.io/services/api-gateway/graphql',
+    uri: 'http://mini-mes.digital/services/api-gateway/graphql',
     request: (operation) => {
         const token = localStorage.getItem('token');
         operation.setContext({
